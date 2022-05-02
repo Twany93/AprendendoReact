05220789443bbf66bee56977ab43fd4eb785c521
Twany93 <twany.rocha@hotmail.com>
2022-05-02 12:32:36 -0300

    Atividade React 2

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Home from './paginas/home/Home'
+import './App.css';
+
+
+// todo esse conteúdo dentro da função são as tags html que serão renderizadas pela index App.tsx
+
+// dentro de uma funçaõ JavaScript, tenho  tem conteúdo html, devido a uma extensão de sintaxe jsx, no caso também utilizamos o typescript, logo arquivo deve ser salvo como .tsx
+
+
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        // <> : fragment, uma forma abreviada utilizado para retornar uma lista de elementos
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://uploads.metropoles.com/wp-content/uploads/2022/05/01151609/Festa-da-comemoracao-do-titulo-do-Real-Madrid-600x400.jpeg" alt="Don Carlo Ancelotti" className="img"/>
+        </>
+    );
+
+}
+
+export default Home;
